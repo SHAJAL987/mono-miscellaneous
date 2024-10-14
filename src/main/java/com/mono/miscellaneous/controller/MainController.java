34f6dcd67ad59cd0769a26e5b998803c9f418ecc
MiddlewareEXP <167507165+MiddlewareEXP@users.noreply.github.com>
2024-10-14 20:23:57 +0600

    feat:logging removed

--- src/main/java/com/mono/miscellaneous/controller/MainController.java
+++ src/main/java/com/mono/miscellaneous/controller/MainController.java
@@ -1,6 +1,5 @@
 package com.mono.miscellaneous.controller;
 
-import com.mono.miscellaneous.common.controllers.BaseMiscellaneousController;
 import com.mono.miscellaneous.common.utilities.CommonEnum;
 import com.mono.miscellaneous.payload.ErrorObject;
 import com.mono.miscellaneous.payload.IpLookupRequest;
@@ -8,17 +7,35 @@ import com.mono.miscellaneous.payload.IpLookupResponse;
 import com.mono.miscellaneous.payload.RealtimeWeatherResponse;
 import com.mono.miscellaneous.service.RapidApiService;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 @RequestMapping("/miscellaneous/v1")
-public class MainController extends BaseMiscellaneousController {
+public class MainController{
+    @Autowired
     private RapidApiService rapidApiService;
 
-    public MainController(RapidApiService rapidApiService) {
-        this.rapidApiService = rapidApiService;
+    private final SimpleDateFormat ft3 = new SimpleDateFormat("yyDDDHHmm");
+    private final SimpleDateFormat ft4 = new SimpleDateFormat("yyyyMMddHHmmss");
+
+    private static AtomicInteger sequenceNumber = new AtomicInteger(0);
+
+    protected String getServiceID() {
+        Date myDate = new Date();
+        String sequenceString = String.format("%04d", sequenceNumber.getAndIncrement() % 1000);
+        return ft3.format(myDate) + sequenceString;
+    }
+
+    protected String getTimeStamp() {
+        Date myDate = new Date();
+        return ft4.format(myDate);
     }
 
     @GetMapping("/checkHealth")
@@ -35,7 +52,6 @@ public class MainController extends BaseMiscellaneousController {
         reqObj.put("lat",lat);
         reqObj.put("lon",lon);
 
-        logRequest(reqObj);
         ResponseEntity response;
         try{
             RealtimeWeatherResponse objResponse = rapidApiService.getRealTimeWeather(lat, lon, getServiceID());
@@ -44,10 +60,10 @@ public class MainController extends BaseMiscellaneousController {
             }else {
                 response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorObject(getServiceID(), CommonEnum.ResponseCode.NOT_FOUND.getCode(), CommonEnum.ResponseCode.NOT_FOUND.getMessage(),getTimeStamp()));
             }
-            logResponse(response.getBody());
             return response;
         }catch (Exception e){
-            return handleException(e);
+            response = ResponseEntity.internalServerError().body(e.getMessage());
+            return response;
         }
     }
 
@@ -63,7 +79,6 @@ public class MainController extends BaseMiscellaneousController {
         reqObj.put("apiKey",apiKey);
         reqObj.put("apiHost",apiHost);
 
-        logRequest(reqObj);
         ResponseEntity response;
         try{
             IpLookupResponse objResponse = rapidApiService.getIplookup(ipLookupRequest,apiKey,apiHost,getServiceID());
@@ -71,11 +86,11 @@ public class MainController extends BaseMiscellaneousController {
                 response = ResponseEntity.ok(objResponse);
             }else {
                 response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorObject(getServiceID(), CommonEnum.ResponseCode.NOT_FOUND.getCode(), CommonEnum.ResponseCode.NOT_FOUND.getMessage(),getTimeStamp()));
-            }
-            logResponse(response.getBody());
+            };
             return response;
         }catch (Exception e){
-            return handleException(e);
+            response = ResponseEntity.internalServerError().body(e.getMessage());
+            return response;
         }
     }
 }
